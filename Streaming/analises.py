9e547690518da0c5248d18cfac8b439d6fb504b0
franciscocassol <francisco.cassol@edu.pucrs.br>
2025-10-07 23:07:02 -0300

    create_match (inovacao e criatividade)

--- Streaming/analises.py
+++ Streaming/analises.py
@@ -24,3 +24,36 @@ class Analises:
     @staticmethod
     def total_reproducoes(usuarios: list[Usuario]) -> int:
         pass
+
+    @staticmethod
+    def create_match_playlists(u1: Usuario, u2: Usuario):
+        musicas_compativeis = []
+        musicas_em_comum = set(u1.historico.keys()) & set(u2.historico.keys())
+
+        if not musicas_em_comum:
+            print("Não há musicas em comum no historico dos usuarios para criar um match")
+            return None
+        
+        for musica in musicas_em_comum:
+            avaliacoes_u1 = u1.historico.get(musica, [])
+            avaliacoes_u2 = u2.historico.get(musica, [])
+
+            if avaliacoes_u1 and avaliacoes_u2:
+                media_u1 = sum(avaliacoes_u1) / len(avaliacoes_u1)
+                media_u2 = sum(avaliacoes_u1) / len(avaliacoes_u2)
+            
+            if abs(media_u1 - media_u2) <= 1:
+                musicas_compativeis.append(musica)
+
+        if not musicas_compativeis:
+            print("Não há musicas com avaliações compativeis para criar um match.")
+            return None
+        
+        return Playlist(
+            nome = f"Match de {u1.nome} e {u2.nome}",
+            user = u1
+            itens= musicas_compativeis
+        )
+
+
+    
